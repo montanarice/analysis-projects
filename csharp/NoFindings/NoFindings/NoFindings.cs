4eb629e025a4d1d09f106074fe6815edb00f3118
David Rice <davidrice363@gmail.com>
2018-01-26 11:31:41 -0700

    Minor updates to NoFindings

--- csharp/NoFindings/NoFindings/NoFindings.cs
+++ csharp/NoFindings/NoFindings/NoFindings.cs
@@ -1,14 +1,19 @@
-﻿namespace NoFindings
+﻿using System;
+
+[assembly:CLSCompliant(true)]
+namespace NoFindings
 {
     /// <summary>
-    /// C# project designed to have no Scanner findings
+    /// C# project designed to have no Scanner findings when using the Quamoco
+    /// "Hello World" quality model.
+    /// 
+    /// Here are more comment lines to reach minimum comment threshold density.
     /// </summary>
-    class NoFindings
+    public class NoFindings
     {
-        static void Main(string[] args)
+        public static void Main(String[] args)
         {
-            int myVariable = 0;
-            myVariable++;
+            String someString = String.Empty;
         }
     }
 }
