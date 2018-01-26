75ec34b6cf2a2315f89d109d2a2b7abe43f9aae7
David Rice <davidrice363@gmail.com>
2018-01-26 11:00:51 -0700

    Added NoFindings C# project

---
+++ csharp/NoFindings/NoFindings/NoFindings.cs
@@ -0,0 +1,14 @@
+﻿namespace NoFindings
+{
+    /// <summary>
+    /// C# project designed to have no Scanner findings
+    /// </summary>
+    class NoFindings
+    {
+        static void Main(string[] args)
+        {
+            int myVariable = 0;
+            myVariable++;
+        }
+    }
+}
